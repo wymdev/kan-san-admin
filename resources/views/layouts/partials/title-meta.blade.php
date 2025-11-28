1de1e119d056288403891769a9b232530ea9de34
wymdev <waiyanmaing.dev@gmail.com>
2025-11-28 14:36:10 +0700

    csrf token add

--- resources/views/layouts/partials/title-meta.blade.php
+++ resources/views/layouts/partials/title-meta.blade.php
@@ -2,5 +2,6 @@
 <title>{{ $title }} | Kan San Application</title>
 <meta content="width=device-width, initial-scale=1.0" name="viewport" />
 <meta content="A Fully Featured Lottery Ticket System" name="description" />
+<meta name="csrf-token" content="{{ csrf_token() }}" />
 <!-- App favicon -->
 <link href="/images/logo-bg.png" rel="shortcut icon" />
